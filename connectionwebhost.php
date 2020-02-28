941d930d7271f4a2e0dca6b82e2d853c6b96d643
Nathaniel Dulo <jndulo@alouattagroup.com>
2020-02-28 16:44:52 +0800

    added connection.php for the webhost setup

---
+++ connectionwebhost.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "id12685021_jnhkaratedo";
+$password = "@jnhkaratedo";
+$dbname = "id12685021_jnh_db";
+
+$con = mysqli_connect($servername,$username,$password,$dbname);
+
+	if(!$con){
+		die('Please Check your Connection' .mysqli_error());
+	}
+
+
+?>
